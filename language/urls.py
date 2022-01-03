c6250277d25befdcc95cef2942fbeff2211da779
zahur <zahurmeerun@hotmail.com>
2022-01-03 16:37:12 +0400

    add: boiletplate settings

--- language/urls.py
+++ language/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("home.urls")),   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
